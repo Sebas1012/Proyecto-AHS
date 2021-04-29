82ab5dcbdad123abf0974f42922690bc9002415a
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-04-28 23:39:58 -0500

    Codigo sensor v1

--- sensor.ino
+++ sensor.ino
@@ -1,9 +1,35 @@
+#include "DHT.h"
+
+#define DHTPIN 12
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup()
 {
-	
+	Serial.begin(9600);
+    dht.begin();
 }
 
 void loop()
 {
-	
+	delay(5000);
+
+    float humedad = dht.readHumidity();
+    float temperatura = dht.readTemperature();
+
+    if (isnan(humedad) || isnan(temperatura)){
+        Serial.println("Error");
+        return;
+    }
+
+    Serial.print("Humedad: ");
+    Serial.print(humedad);
+    Serial.print("%");
+    Serial.print("\n");
+    Serial.print("Temperatura: ");
+    Serial.print(temperatura);
+    Serial.print("°C");
+    Serial.print("\n");
+
 }
