beb45177d342c5029b6003fdcbf3ac486960715e
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-05-25 23:01:31 -0500

    Estricura de datos en HTML

--- server/public/data.js
+++ server/public/data.js
@@ -2,4 +2,7 @@ const sockets = io();
 
 sockets.on('temperatura', function(line) {
     console.log(line);
+
+    let temperatura = document.getElementById('temp');
+    temperatura.innerHTML = line;
 })
