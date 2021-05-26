5e5df92ab861f4b5e55dce2dd67bb387fde1b898
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-05-25 21:31:01 -0500

    Correccion en conexion serial y creacion de servidro http con express

---
+++ server/index.js
@@ -0,0 +1,35 @@
+//Servidor HTTP y websockets
+
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+// const io = socketIO.listen(server);
+
+app.use(express.static(__dirname + '/public'))
+
+server.listen(3000, function(){
+    console.log('Servidor listo en:', 3000);
+});
+
+
+
+// Conexion al puerto serial
+const { parse } = require('cookie');
+const serialPort = require('serialport');
+
+const port = new serialPort(
+    "COM4",
+    {baudRate: 9600}
+);
+
+const parser = new serialPort.parsers.Readline();
+
+port.pipe(parser);
+
+parser.on('data', (line)=>{
+    console.log(line);
+});
+
