393720fad47fd7f595fa7b4e8869eb198d5f8d88
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-05-25 19:08:04 -0500

    conexion con serialport.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-const SerialPort = require('serialport');
+const { parse } = require('cookie');
+const serialPort = require('serialport');
+
+const port = new serialPort(
+    'COM3',
+    {baudRate: 9600}
+);
+
+const parser = new serialPort.parsers.Readline();
+
+port.pipe(parser);
+
+parser.on('data', (line)=>{
+    console.log('line');
+});
+
