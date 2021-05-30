563db06e25b649d854dcf2b6e0d571dea17a1011
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-05-30 13:52:28 -0500

    Conversion a grados fahrenheit

--- server/public/js/data.js
+++ server/public/js/data.js
@@ -5,6 +5,10 @@ sockets.on('temperatura', function(str) {
     let hmd = str.substr(0, 2);
     let tmp = str.substr(5, 2);
 
+    tmp = parseFloat(tmp);
+
+    let frh = (tmp * 9/5) + 32;
+
     // console.log(tmp);
 
     let temperatura = document.getElementById('temp');
@@ -16,11 +20,9 @@ sockets.on('temperatura', function(str) {
     let temperatura_completa = document.getElementById('complete_temp');
     let humedad_completa = document.getElementById('complete_hum');
 
-    let cm_hmd = str.substr(0, 5);
-    let cm_tmp = str.substr(5, 5);
 
-    temperatura_completa.innerHTML = cm_tmp + "°C";
-    humedad_completa.innerHTML = cm_hmd + "%";
+    temperatura_completa.innerHTML = frh + "°F";
+    humedad_completa.innerHTML = hmd + "%";
 
 
 })
